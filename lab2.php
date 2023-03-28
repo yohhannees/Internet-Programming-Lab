44ff435f59a93ecfea8ad98d2b879a21f702eb78
Yohannees <yohannesgetachewerieso@gmail.com>
2023-03-28 10:27:11 +0300

    lab2

--- lab2.php
+++ lab2.php
@@ -7,56 +7,73 @@
     <title>lab2</title>
 </head>
 <body>
-    <h1>arrays<h1>
+   
     <?php
-  
-  //indexed array
-   echo "indexed array";
-   echo"<br>";
-   echo"<br>";
-    $cars = array("Volvo", "BMW", "Toyota");
-    //display each in new line using loop
-    foreach ( $cars as $car) {
-        echo $car;
-        echo "\n";
-        echo "<br>";
-    }
-     echo "<br>";
-    //associative array
-    echo "associative array";
-    echo"<br>";
-   echo"<br>";    
-    $age = array("befikadu"=>"35", "eyob"=>"97", "ashenafi"=>"43");
-    //display each in new line using loop
-    foreach ( $age as $key => $value) {
-        echo $key;
-        echo " ";
-        echo $value;
-        echo "<br>";
-    }
+//   echo"arrays";
+//   //indexed array
+//    echo "indexed array";
+//    echo"<br>";
+//    echo"<br>";
+//     $cars = array("Volvo", "BMW", "Toyota");
+//     //display each in new line using loop
+//     foreach ( $cars as $car) {
+//         echo $car;
+//         echo "\n";
+//         echo "<br>";
+//     }
+//      echo "<br>";
+//     //associative array
+//     echo "associative array";
+//     echo"<br>";
+//    echo"<br>";    
+//     $age = array("befikadu"=>"35", "eyob"=>"97", "ashenafi"=>"43");
+//     //display each in new line using loop
+//     foreach ( $age as $key => $value) {
+//         echo $key;
+//         echo " ";
+//         echo $value;
+//         echo "<br>";
+//     }
 
-     echo"<br>"; 
-   // sort 
-   sort($cars);
-   echo "<after sorting";
-   echo "</after>";
-       foreach ( $cars  as $car) {
-        echo $car;
-        echo "<br>";
-    }
+//      echo"<br>"; 
+//    // sort 
+//    sort($cars);
+//    echo "<after sorting";
+//    echo "</after>";
+//        foreach ( $cars  as $car) {
+//         echo $car;
+//         echo "<br>";
+//     }
 
-     array_push($cars,"vitz" );
+//      array_push($cars,"vitz" );
 
-     //after push
-     echo "after push";
-     echo "<br>";
+//      //after push
+//      echo "after push";
+//      echo "<br>";
       
-      foreach ( $cars as $car) {
-        echo $car;
-        echo "\n";
-        echo "<br>";
-    }
-    echo "<br>";
+//       foreach ( $cars as $car) {
+//         echo $car;
+//         echo "\n";
+//         echo "<br>";
+//     }
+//     echo "<br>";
+//         echo "<br>";
+
+//         echo "global variable";
+//         echo "<br>";
+
+//         $x = 5;
+//         $y = 10;
+//         function addition(){
+//             $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+//         };
+//          addition();
+//         echo "$z";
+   //request method
+   
+
+
+
 
 ?>
 </body>
