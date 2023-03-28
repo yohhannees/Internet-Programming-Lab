e85fb74b2c8090a3f91b209e9d92ef46e90e4614
Yohannees <yohannesgetachewerieso@gmail.com>
2023-03-28 10:43:30 +0300

    lab2

--- lab2.php
+++ lab2.php
@@ -9,72 +9,80 @@
 <body>
    
     <?php
-//   echo"arrays";
-//   //indexed array
-//    echo "indexed array";
-//    echo"<br>";
-//    echo"<br>";
-//     $cars = array("Volvo", "BMW", "Toyota");
-//     //display each in new line using loop
-//     foreach ( $cars as $car) {
-//         echo $car;
-//         echo "\n";
-//         echo "<br>";
-//     }
-//      echo "<br>";
-//     //associative array
-//     echo "associative array";
-//     echo"<br>";
-//    echo"<br>";    
-//     $age = array("befikadu"=>"35", "eyob"=>"97", "ashenafi"=>"43");
-//     //display each in new line using loop
-//     foreach ( $age as $key => $value) {
-//         echo $key;
-//         echo " ";
-//         echo $value;
-//         echo "<br>";
-//     }
+  echo"arrays";
+  //indexed array
+   echo "indexed array";
+   echo"<br>";
+   echo"<br>";
+    $cars = array("Volvo", "BMW", "Toyota");
+    //display each in new line using loop
+    foreach ( $cars as $car) {
+        echo $car;
+        echo "\n";
+        echo "<br>";
+    }
+     echo "<br>";
+    //associative array
+    echo "associative array";
+    echo"<br>";
+   echo"<br>";    
+    $age = array("befikadu"=>"35", "eyob"=>"97", "ashenafi"=>"43");
+    //display each in new line using loop
+    foreach ( $age as $key => $value) {
+        echo $key;
+        echo " ";
+        echo $value;
+        echo "<br>";
+    }
 
-//      echo"<br>"; 
-//    // sort 
-//    sort($cars);
-//    echo "<after sorting";
-//    echo "</after>";
-//        foreach ( $cars  as $car) {
-//         echo $car;
-//         echo "<br>";
-//     }
+     echo"<br>"; 
+   // sort 
+   sort($cars);
+   echo "<after sorting";
+   echo "</after>";
+       foreach ( $cars  as $car) {
+        echo $car;
+        echo "<br>";
+    }
 
-//      array_push($cars,"vitz" );
+     array_push($cars,"vitz" );
 
-//      //after push
-//      echo "after push";
-//      echo "<br>";
+     //after push
+     echo "after push";
+     echo "<br>";
       
-//       foreach ( $cars as $car) {
-//         echo $car;
-//         echo "\n";
-//         echo "<br>";
-//     }
-//     echo "<br>";
-//         echo "<br>";
+      foreach ( $cars as $car) {
+        echo $car;
+        echo "\n";
+        echo "<br>";
+    }
+    echo "<br>";
+        echo "<br>";
 
-//         echo "global variable";
-//         echo "<br>";
+        echo "global variable";
+        echo "<br>";
 
-//         $x = 5;
-//         $y = 10;
-//         function addition(){
-//             $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
-//         };
-//          addition();
-//         echo "$z";
-   //request method
-   
-
+        $x = 5;
+        $y = 10;
+        function addition(){
+            $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+        };
+         addition();
 
 
 
 ?>
+
+        <form method ="post" action="lab2display.php">
+        <input type ="text"=name="fname"></br>
+        <input type="text" name="sname">
+        <button type="submit">Add</button>
+        </form>
+
+        <php echo $_REQUEST['fname']?>
+            
+        <php echo $_REQUEST['sname']?>
+
+
 </body>
 </html>
---
+++ lab2display.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lab2display</title>
+</head>
+<body>
+    <php 
+    echo $_REQUEST['fname'];
+      ?>
+            
+        <php 
+        echo $_REQUEST['sname'];
+        ?>
+
+        
+</body>
+</html>
