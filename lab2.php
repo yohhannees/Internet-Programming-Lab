24690251ed61bc501ecbe1db18bbdce54e28fbc0
Yohannees <yohannesgetachewerieso@gmail.com>
2023-03-28 09:06:57 +0300

    updated

---
+++ lab2.php
@@ -0,0 +1,13 @@
+<?php
+  
+  //indexed array
+    $cars = array("Volvo", "BMW", "Toyota");
+    //display each in new line using loop
+    foreach ( $cars as $car) {
+        echo $car;
+        echo "\n";
+    }
+
+
+
+?>
